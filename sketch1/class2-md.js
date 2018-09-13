8f8111a2db948dc0d12da99acca075e91bd85267
olasosz <43154874+olasosz@users.noreply.github.com>
2018-09-13 11:36:32 -0400

    colours and movement work now

--- sketch1/class2-md.js
+++ sketch1/class2-md.js
@@ -4,16 +4,17 @@ function setup(){
 	createCanvas(720,480);
 	var button = createButton('add particle');
 	button.mousePressed(addParticle);
+	noStroke();
 }
 
 function draw() {
-	noStroke();
-	background(60,100,245,15);
+	 background(60,100,245,15);
 
 	for (var i = 0; i < mouseClicks.length; i++) {
+		fill(mouseClicks[i].color);
 		ellipse(mouseClicks[i].x,mouseClicks[i].y,10);
 		mouseClicks[i].x=mouseClicks[i].x+mouseClicks[i].xspeed;
-		mouseClicks[i].y=mouseClicks[i].y+mouseClicks[i].speed;
+		mouseClicks[i].y=mouseClicks[i].y+mouseClicks[i].yspeed;
 	}
 }
 
@@ -22,7 +23,8 @@ function addParticle() {
 		x:random(width),
 		y:random(height),
 		xspeed:random(-5,5),
-		yspeed:random(-5,5)
+		yspeed:random(-5,5),
+		color: color(random(100,150), random(255),random(255))
 	}
 	mouseClicks.push(clickposition);
 }
