58185d5e07617a1a5efec51868772c45ef9e111a
olasosz <43154874+olasosz@users.noreply.github.com>
2018-09-14 13:19:04 -0400

    started last sketch - dont know what to use to make a server

---
+++ Assignment1/sketch5/sketch5.js
@@ -0,0 +1,27 @@
+//adapted from https://p5js.org/reference/#/p5/loadImage
+
+var img;
+
+function preload() {
+  img = loadImage('clicker.png');
+}
+
+function setup() {
+	createCanvas(720,480);
+	nostroke();
+	image(img,random(width),random(height));
+
+}
+
+function draw() {
+	background(250);
+}
+
+function mouseClicked() {
+
+var click = dist(img.x,img.y,mouseX,mouseY);
+		if (click < bubble.r) {
+			image(img,random(width),random(height));
+		}
+	}
+}
